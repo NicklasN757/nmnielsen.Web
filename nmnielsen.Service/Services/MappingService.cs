b1b57b68a3aee66643ba4b7ae4a6393480e27fe3
Nicklas Markvart Nielsen <nicklas.m.nielsen@live.com>
2021-12-24 15:48:37 +0100

    Added Base and Mapping services

---
+++ nmnielsen.Service/Services/MappingService.cs
@@ -0,0 +1,32 @@
+﻿namespace nmnielsen.Service.Services
+{
+    /// <summary>
+    /// The MappingService. Used for Automapper so only 1 mapper is needed.
+    /// </summary>
+    public class MappingService : BaseService
+    {
+        public readonly AutoMapper.IMapper _mapper;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MappingService"/> class.
+        /// </summary>
+        public MappingService()
+        {
+            AutoMapper.MapperConfiguration mapperConfig = new AutoMapper.MapperConfiguration(cfg =>
+            {
+                #region Class Mappings
+                
+                #endregion
+            });
+
+            try
+            {
+                _mapper = mapperConfig.CreateMapper();
+            }
+            catch (Exception ex)
+            {
+                LogError("Failed to create mappings", ex);
+            }
+
+        }
+    }
+}
