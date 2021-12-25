816bb459b8ba56d7945dd93dac9af4f3d70677fb
Nicklas Markvart Nielsen <nicklas.m.nielsen@live.com>
2021-12-25 01:50:10 +0100

    Added Project entiti and some frontend. Made some small changes.

---
+++ nmnielsen.Repository/Entities/Project.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace nmnielsen.Repository.Entities
+{
+    public class Project
+    {
+        /// <summary>
+        /// The project id
+        /// </summary>
+        [Required]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The project name
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The project description
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// The project start date
+        /// </summary>
+        public DateTime StartDate { get; set; }
+
+        /// <summary>
+        /// The project end date
+        /// </summary>
+        public DateTime EndDate { get; set; }
+
+        /// <summary>
+        /// The project isDeleted variable
+        /// </summary>
+        [Required]
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ nmnielsen.Web/Pages/Project/ProjectDetails.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace nmnielsen.Web.Pages.Project
+{
+    public class ProjectDetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
